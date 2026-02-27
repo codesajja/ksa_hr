beb3846ac1a2983032ec8cf3a150380f7d440a99
Sajjad Ahmed <sajjadsajju9050@gmail.com>
2026-02-27 07:00:58 +0000

    Updated employee loan

---
+++ ksa_hr/ksa_hr/doctype/employee_loan/employee_loan.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Sajjad and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class EmployeeLoan(Document):
+	pass
